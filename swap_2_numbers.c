8d3cb02a64ca1e856623948f641e1e0d137f54d5
pawansingh1610 <pawansingh1610@gmail.com>
2019-10-03 23:31:09 +0530

    Update swap_2_numbers.c
    without using third variable

--- swap_2_numbers.c
+++ swap_2_numbers.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
 int main()
 {
-      int firstNumber, secondNumber, temporaryVariable;
+      int firstNumber, secondNumber;
       printf("Enter first number: ");
       scanf("%d", &firstNumber);
       printf("Enter second number: ");
       scanf("%d",&secondNumber);
-      // Value of firstNumber is assigned to temporaryVariable
-      temporaryVariable = firstNumber;
-      // Value of secondNumber is assigned to firstNumber
-      firstNumber = secondNumber;
-      // Value of temporaryVariable (which contains the initial value of firstNumber) is assigned to secondNumber
-      secondNumber = temporaryVariable;
+      //without using third variable
+      firstNumber=firstNumber+secondNumber;
+      secondNumber=firstNumber-secondNumber;
+      firstNumber=firstNumber-secondNumber;
       printf("\nAfter swapping, firstNumber = %d\n", firstNumber);
       printf("After swapping, secondNumber = %d", secondNumber);
       return 0;
