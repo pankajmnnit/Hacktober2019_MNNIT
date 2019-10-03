bba5020d014dfe6055b15e3018c6e35f009cf975
pawansingh1610 <pawansingh1610@gmail.com>
2019-10-03 23:46:02 +0530

    Update factors.c
    just ++i to i++

--- factors.c
+++ factors.c
@@ -7,7 +7,7 @@ int main()
     printf("Enter a positive integer: ");
     scanf("%d",&number);
     printf("Factors of %d are: ", number);
-    for(i=1; i <= number; ++i) {
+    for(i=1; i <= number; i++) {
         if (number%i = 0) 
             printf("%d ",i);
         }
