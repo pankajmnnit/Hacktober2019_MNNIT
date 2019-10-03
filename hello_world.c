22d6d30173a3e7948e1d2aa934255f4565e49238
pawansingh1610 <pawansingh1610@gmail.com>
2019-10-03 23:37:16 +0530

    Update hello_world.c
    hello world without main function just to make task bit cozy

--- hello_world.c
+++ hello_world.c
@@ -1,11 +1,15 @@
-
-
-
-#include <stdio.h>
-
-int main()
-{
-		printf("this code is wrong");
-		return 0;
-		      }	      
-
+#include<stdio.h>   
+int no_main();  
+  
+void fun(){ 
+  
+    // calling function to print 
+    no_main();  
+    exit(0); 
+} 
+  
+int no_main() 
+{ 
+    printf("printing wthout uisng main"); 
+    return 0; 
+} 
